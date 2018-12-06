279540ab4614f6f296b5c652620ea64e4f20789c
Salvatore Nicosia <nicosia.salvo24@gmail.com>
2018-12-06 14:06:11 -0800

    Changed API BASE

--- src/app/util/globals.ts
+++ src/app/util/globals.ts
@@ -3,7 +3,7 @@ import { HttpHeaders } from '@angular/common/http';
 
 export const globals = {
   // API
-  API_BASE: environment.API_BASE || 'localhost:5000/api',
+  API_BASE: 'https://k3jlseejfb.execute-api.us-east-1.amazonaws.com/dev' || 'localhost:5000/api',
   get BASE() {
     return this.API_BASE + this.API_VERSION;
   },
